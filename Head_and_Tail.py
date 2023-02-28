8aeb7cc42431a7547695c041ab577173fdf0c6d2
joshsmiththenoob <joshsmith0223029@gmail.com>
2023-02-28 14:28:24 +0800

    added select method from Head_and_Tail.py

--- Head_and_Tail.py
+++ Head_and_Tail.py
@@ -23,6 +23,7 @@ print("Tail最後兩筆資料 : \n", new_table)
 print("="*50)
 
 # select specific column and index to get data
+# one column
 print("my_table['name'] : ","取得單一欄位資料(型別為Series)")
 print(my_table["name"])
 print(type(my_table["name"]))
@@ -33,12 +34,27 @@ print(my_table[["name"]])
 print(type(my_table[["name"]]))
 print("="*50)
 
-
+# multiple columns
 print("my_table[['name','chinese']] : ","取得多欄位資料(型別為DataFrame)")
 print(my_table[["name","chinese"]])
 print("="*50)
 
-print("取得索引值(index)0~2的紀錄(資料)")
+# index
+print("my_table[0:3] : 取得索引值(index)0~2的紀錄(資料)")
 print(my_table[0:3])
+print("="*50)
 
-print("at['index','column'] : 特定筆數、特定欄位的資料")
+# at[index,'column name']
+print("at[index,'column name'] : 特定筆數、特定欄位的資料")
+print(my_table.at[1,"math"])
+print("="*50)
+
+# iat[index, column index]
+print("iat[index, column index] : 第幾筆、第幾個欄位的資料")
+print(my_table.iat[1,1])
+print("="*50)
+
+# loc[[index0:index1],['column name1', 'column name2']]
+print("loc[[index0, index1],['column name1', 'column name2']] : 查詢特定範圍(index、colum name)的資料")
+print(my_table.loc[[0,2],["name","math"]])
+print("="*50)
